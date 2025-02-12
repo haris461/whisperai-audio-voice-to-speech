47178a464b63c422862f15b6c834440f0b0ecbc8
haris461 <158203393+haris461@users.noreply.github.com>
2025-02-12 20:04:35 +0500

    Add files via upload

---
+++ audio_config.py
@@ -0,0 +1,6 @@
+import sounddevice as sd
+
+# Set the correct microphone index
+sd.default.device = 16  # Change this to your correct mic index
+
+print(f"Microphone set to device index: {sd.default.device}")
